1f3e8e36c1e73af12df1df086764f38b30451578
Devanshi Doshi <76578430+Devanshi-Crypto@users.noreply.github.com>
2022-12-05 15:29:18 +0530

    padding-container update

--- src/app.js
+++ src/app.js
@@ -1,11 +1,3 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
 const bars = document.querySelector('.fa-bars');
 const navbar = document.querySelector('.navbar');
 
@@ -16,7 +8,6 @@ bars.addEventListener('click',()=>{
 
 $(window).on('scroll load',function(){
     bars.classList.toggle('fa-times');
-    navbar.classList.toggle('nav-toggle');
 
     if($(window).scrollTop() > 30){
         $('header').addClass('header-active');
